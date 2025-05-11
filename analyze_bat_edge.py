1dabe615abdeaf85307a97f485a1ab14dc8806a5
Maheen-Nadeem <153733343+Maheen-Nadeem@users.noreply.github.com>
2025-05-11 18:05:53 +0500

    Update analyze_bat_edge.py
    This portion of code would forward our module's output to the next module, i.e trajectory analysis module. The response would be displayed in a post request on Postman

--- analyze_bat_edge.py
+++ analyze_bat_edge.py
@@ -1,14 +1,13 @@
 # Forwarding to another module
-        # import requests
-        # trajectory_api = "http://localhost:5001/analyze_trajectory"
-        # response = requests.post(trajectory_api, json=output_payload)
-        # print("Forwarded to trajectory module. Response:", response.text)
+        trajectory_api = "http://localhost:5001/analyze_trajectory"
+        response = requests.post(trajectory_api, json=output_payload)
+        print("Forwarded to trajectory module. Response:", response.text)
 
         return jsonify({
             "status": "success",
             "bat_edge_detected": detection["bat_edge_detected"],
-            "forwarded_to_trajectory": False,
-            "trajectory_module_response": None
+            "forwarded_to_trajectory": True,
+            "trajectory_module_response": response.json()
         }), 200
 
     except Exception as e:
